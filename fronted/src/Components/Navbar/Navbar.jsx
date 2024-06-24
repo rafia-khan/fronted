119c7d257b75fddfe63b42e543ef7a859cfb22c5
rafia-khan <khanrafia9218@gmail.com>
2024-06-25 03:39:43 +0600

    update

--- fronted/src/Components/Navbar/Navbar.jsx
+++ fronted/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+
+
   return (
-    <div>
-      Navbar.navbar
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPER</p>
+      </div>
+      <ul className="nav-manu"> 
+            <li>Shop <hr/></li>
+            <li>Men</li>
+            <ii>Women</ii>
+            <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>login</button>
+        <img src={cart_icon} alt=""/>
+        <div className="nav-cart-count">0</div>
+      </div>
+      
     </div>
   )
 }
