0b34c75d0bd761265efc4b0ea7e191fe8701ae58
rafia-khan <khanrafia9218@gmail.com>
2024-06-24 19:14:16 +0600

    first commit

---
+++ fronted/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+  return (
+
+      
+      <div className="card">
+        <Navbar/>
+        </div>
+        
+  )
+}
+
+export default App
