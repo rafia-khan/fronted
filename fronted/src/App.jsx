99170036ceb598b06e528c4725cdf47770ee9e50
rafia-khan <khanrafia9218@gmail.com>
2024-06-25 18:08:50 +0600

    update

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 function App() {
 
   return (
 
       
-      <div className="card">
+      <div>
         <Navbar/>
         </div>
         
