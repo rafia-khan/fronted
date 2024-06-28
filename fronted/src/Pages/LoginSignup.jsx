b07686a012f69391d7833b89c0d04ab99cb50395
rafia-khan <khanrafia9218@gmail.com>
2024-06-29 04:54:43 +0600

    rafia

--- fronted/src/Pages/LoginSignup.jsx
+++ fronted/src/Pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
+    <div className='loginsignup'>
+      <div className='loginsignup-container'>
+         <h1>Sign Up</h1>
+         <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Nmame'/>
+          <input type="email" placeholder='Email Adress'/>
+          <input type="password" placeholder='password' />
+          </div>
+          <button>Coutinue</button>
+          <p className='loginsignup-login'>Already have an account?<span>Login here</span></p>
+          <div className="loginsighup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>By continuing, i agree to the terms of use & privacy policy.</p>
+
+          </div>
+      </div>
       
     </div>
   )
