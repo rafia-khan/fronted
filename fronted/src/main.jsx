26f36cbc48b47a16099ac2164f0afd646e056149
rafia-khan <khanrafia9218@gmail.com>
2024-06-28 19:21:44 +0600

    update

--- fronted/src/main.jsx
+++ fronted/src/main.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ShopContextProvider from './Context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ShopContextProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </ShopContextProvider>
 )
